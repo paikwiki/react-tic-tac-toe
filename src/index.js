68d6a8489559196682523fb8b8923c5830c7cfe5
paikwiki <paikwiki@gmail.com>
2018-05-25 19:03:40 +0900

    Write todo list with comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/* 작성해야 하는 목록
+// ==========================
+// 1. Square: 클래스에서 function으로 변경
+// 1. Square: props로 값 할당
+// 1. Board: 현재 플레이어를 표시하는 .status를 Game에 구현
+// 1. Game: history 라는 키에 Board의 상태를 저장하고 있는 history 객체 할당 -> slice() 사용
+// 1. Game: 플레이어가 클릭할 때마다 history 업데이트 -> concat 매서드 사용
+// 1. Game: xIsNext 값을 통해 지금 플레이할 플레이어를 판단
+// 1. winner?? 헬퍼 함수로 승패 판단
+// 1. Game: 각 플레이어의 턴 변경
+// 1. Game: .game-info에 화면의 현재 상태를 목록 안의 버튼으로 출력(시간 여행) -> map 매서드 사용
+// 1. Game: 시간 여행 클릭시 당시 상태로 돌아감 -> 다음 플레이어 체크 필요합
+*/
+
 class Square extends React.Component {
   render() {
     return (
